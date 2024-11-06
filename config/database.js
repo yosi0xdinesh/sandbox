e47812eb42898b553788c6f4f86acfff4c71f8ca
yosi0xdinesh <dineshkumar.n@yosicare.com>
2024-11-06 15:59:02 +0530

    vercel file

--- config/database.js
+++ config/database.js
@@ -1,5 +1,7 @@
 const Sequelize = require('sequelize');
 const env = require('dotenv');
+const mysql2 = require('mysql2');
+
 env.config();
 
 const DB_HOST = process.env.DB_HOST
@@ -9,7 +11,7 @@ const DB_DATABASE = process.env.DB_DATABASE
 const DB_USERNAME = process.env.DB_USERNAME
 const DB_PASSWORD = process.env.DB_PASSWORD
 const sequelize = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
-  dialect: 'mysql',
+  dialect: mysql2,
   host: DB_HOST
 });
 
