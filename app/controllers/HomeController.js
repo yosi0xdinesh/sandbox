e3b26f9445939774cbddeff3b825bfbe776fc484
yosi0xdinesh <dineshkumar.n@yosicare.com>
2024-11-06 18:26:03 +0530

    vercel file

--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -7,7 +7,7 @@ exports.indexFun = (req, res, next) => {
 }
 exports.updateData = (req, res) => {
 
-	console.log(req);
+	console.log(req.body);
 		const entryId = req.body.id;
 		const editedValue = req.body.editedValue;
 		var data = {
