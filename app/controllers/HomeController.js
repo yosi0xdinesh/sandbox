0474fba9694323a8bf18525dd5c5f8abf0c41701
yosi0xdinesh <dineshkumar.n@yosicare.com>
2024-11-06 17:59:05 +0530

    vercel file

--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -1,5 +1,5 @@
 const User = require('../models/User');
- 
+
 
 exports.indexFun = (req, res, next) => {
 	res.type('html'); // Sets the Content-Type header to text/plain
@@ -7,34 +7,31 @@ exports.indexFun = (req, res, next) => {
 }
 exports.updateData = (req, res) => {
 
-	try{
+ 
 		const entryId = req.body.id;
-	var data = {
-		// id: id,
-		[req.body.columnName]: editedValue,  // Use computed property name syntax
-	};
-	User.update(data, {
-	  where: { id: entryId }
-	})
-	  .then(result => {
-		//   res.send({
-		// 	amount: req.params.amount,
-		// 	level: req.params.level,
-		// 	battleCount: req.params.battleCount,
-		// }) 
-		res.status(200).send({
-			message: "Added " + entryId
-		  });
-		
-	  })
-	  .catch(err => {
-		res.status(500).send({
-		  message: "Error " + entryId
-		});
-	  });
-	} catch (err) {
-		 res.type('text'); // Sets the Content-Type header to text/plain
-		res.send(err);
-	}
-	
-  };
+		var data = {
+			// id: id,
+			[req.body.columnName]: editedValue,  // Use computed property name syntax
+		};
+		User.update(data, {
+			where: { id: entryId }
+		})
+			.then(result => {
+				//   res.send({
+				// 	amount: req.params.amount,
+				// 	level: req.params.level,
+				// 	battleCount: req.params.battleCount,
+				// }) 
+				res.status(200).send({
+					message: "Added " + result
+				});
+
+			})
+			.catch(err => {
+				res.status(500).send({
+					message: "Error " + result
+				});
+			});
+	 
+
+};
