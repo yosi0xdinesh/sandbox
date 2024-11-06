e5fee2402ab17454ef5c2416afb604ca6d475486
yosi0xdinesh <dineshkumar.n@yosicare.com>
2024-11-06 18:57:10 +0530

    vercel file

--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -7,20 +7,21 @@ exports.indexFun = (req, res, next) => {
 }
 exports.updateData = (req, res) => {
 
-	// console.log(req.body);
 
-	try{
+	try {
 		const entryId = req.body.id;
 		const editedValue = req.body.editedValue;
 		var data = {
 			id: entryId,
 			[req.body.columnName]: editedValue,  // Use computed property name syntax
 		};
+		console.log(data);
+
 		User.update(data, {
 			where: { id: entryId }
 		})
 			.then(result => {
-				 
+
 				res.status(200).send({
 					message: "Added " + result
 				});
@@ -31,12 +32,12 @@ exports.updateData = (req, res) => {
 					message: "Error " + result
 				});
 			});
-	} catch(err){
+	} catch (err) {
 		res.status(500).send({
-            message: "Error in updateData: " + err.message
-        });
+			message: "Error in updateData: " + err.message
+		});
 	}
-		
-	 
+
+
 
 };
